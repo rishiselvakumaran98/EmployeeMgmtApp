4fa597ca2d36dc146be801841689bfc560ed12fb
Rishi Selvakumaran <selva053@umn.edu>
2022-07-03 01:36:25 -0400

    Added encrypted properties file

--- src/main/java/com/example/springbootbackend/SpringbootBackendApplication.java
+++ src/main/java/com/example/springbootbackend/SpringbootBackendApplication.java
@@ -2,8 +2,9 @@ package com.example.springbootbackend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
 @SpringBootApplication
+@EnableEncryptableProperties
 public class SpringbootBackendApplication {
 
     public static void main(String[] args) {
